5cd92bd5fa29d82eb6882c5bd93e1e90c9705c77
JuanRubiio <juan12_rubio@hotmail.com>
2018-10-28 16:05:41 +0100

    Subida entidad Endorsement (falta referencia a entidad endorser)

---
+++ src/main/java/domain/Endorsement.java
@@ -0,0 +1,52 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Endorsement extends DomainEntity {
+
+	private Date		moment;
+	private String		comment;
+	private Endorser	writeTo;
+	private Endorser	writeFor;
+
+
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+	@NotBlank
+	public String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(final String comment) {
+		this.comment = comment;
+	}
+	@Valid
+	public Endorser getWriteTo() {
+		return this.writeTo;
+	}
+
+	public void setWriteTo(final Endorser writeTo) {
+		this.writeTo = writeTo;
+	}
+	@Valid
+	public Endorser getWriteFor() {
+		return this.writeFor;
+	}
+
+	public void setWriteFor(final Endorser writeFor) {
+		this.writeFor = writeFor;
+	}
+
+}
