b858b80f9321bbbb0424ecc13328366561dc4bc5
Regina <regy_escalera@hotmail.com>
2018-10-29 21:21:58 +0100

    @Valid in getListReferees
    @Valid in getListReferees

--- src/main/java/domain/Referee.java
+++ src/main/java/domain/Referee.java
@@ -4,12 +4,23 @@ package domain;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.validation.Valid;
 
 @Entity
 public class Referee extends Actor {
 
 	private List<Report>	listReports;
+	private List<Referee>	listReferees;
 
+
+	@Valid
+	public List<Referee> getListReferees() {
+		return this.listReferees;
+	}
+
+	public void setListReferees(final List<Referee> listReferees) {
+		this.listReferees = listReferees;
+	}
 
 	public List<Report> getListReports() {
 		return this.listReports;
