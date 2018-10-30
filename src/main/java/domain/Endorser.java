cc726ba818eba2e8186fab59955209d61424c147
Javier Marin <arioberserker@gmail.com>
2018-10-30 18:54:15 +0100

    Correccion clase Endorser
    Añadido del atributo Endorsement

--- src/main/java/domain/Endorser.java
+++ src/main/java/domain/Endorser.java
@@ -1,6 +1,20 @@
 
 package domain;
 
+import javax.validation.Valid;
+
 public class Endorser extends Actor {
+
+	private Endorsement	endorsement;
+
+
+	@Valid
+	Endorsement getEndorsement() {
+		return this.endorsement;
+	}
+
+	void setEndorsement(final Endorsement endorsement) {
+		this.endorsement = endorsement;
+	}
 
 }
