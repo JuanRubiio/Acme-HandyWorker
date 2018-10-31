d38c0eac821f8165d0597fa48674715034f99af5
Regina <regy_escalera@hotmail.com>
2018-10-31 18:31:42 +0100

    delete @Entity
    delete @Entity

--- src/main/java/domain/Report.java
+++ src/main/java/domain/Report.java
@@ -4,14 +4,12 @@ package domain;
 import java.util.Collection;
 import java.util.Date;
 
-import javax.persistence.Entity;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
-@Entity
 public class Report extends DomainEntity {
 
 	private Date				moment;
