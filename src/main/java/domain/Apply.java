9b45f769714b8e19931d20d9a6ea233930a3c4fc
Carlos <garciaborrego.carlos@gmail.com>
2018-10-31 13:12:47 +0100

    Añadir en las clases @Entity y eliminación de la clase Status
    La relación Apply->Status, pasa a ser como antes. Status es un atributo de Apply.

--- src/main/java/domain/Apply.java
+++ src/main/java/domain/Apply.java
@@ -4,16 +4,20 @@ package domain;
 import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
 public class Apply extends DomainEntity {
 
 	//----------Atributos------------
 	private Date	moment;
+	private String	status;
 	private String	customerComment;
 	private Money	price;
 	private String	handyWorkerComments;
@@ -28,6 +32,16 @@ public class Apply extends DomainEntity {
 
 	public void setMoment(final Date moment) {
 		this.moment = moment;
+	}
+
+	@NotBlank
+	@Pattern(regexp = "PENDING|REJECTED|ACCEPTED")
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(final String status) {
+		this.status = status;
 	}
 
 	@NotBlank
@@ -63,7 +77,6 @@ public class Apply extends DomainEntity {
 	private HandyWorker			handyWorker;
 	private Collection<Phase>	workplan;
 	private FixUpTask			fixUpTask;
-	private Collection<Status>	status;
 
 
 	@NotNull
@@ -93,16 +106,6 @@ public class Apply extends DomainEntity {
 
 	public void setFixUpTask(final FixUpTask fixUpTask) {
 		this.fixUpTask = fixUpTask;
-	}
-
-	@NotNull
-	@Valid
-	public Collection<Status> getStatus() {
-		return this.status;
-	}
-
-	public void setStatus(final Collection<Status> status) {
-		this.status = status;
 	}
 
 }
