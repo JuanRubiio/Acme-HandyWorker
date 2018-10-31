db7aa997b792aa09a0be98b5cb5c6faafb5d4d37
Regina <regy_escalera@hotmail.com>
2018-10-31 18:31:36 +0100

    delete @Entity
    delete @Entity

--- src/main/java/domain/Referee.java
+++ src/main/java/domain/Referee.java
@@ -3,10 +3,8 @@ package domain;
 
 import java.util.Collection;
 
-import javax.persistence.Entity;
 import javax.validation.Valid;
 
-@Entity
 public class Referee extends Actor {
 
 	private Collection<Report>		collectionReports;
